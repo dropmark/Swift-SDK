86b521210141deda4ae2321b079164247c2c9f28
Alex Givens <mail@alexgivens.com>
2020-03-10 14:08:04 -0400

    Remove KeychainSwift

--- Example/Pods/Target Support Files/KeychainSwift-tvOS/KeychainSwift-tvOS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_KeychainSwift_tvOS : NSObject
-@end
-@implementation PodsDummy_KeychainSwift_tvOS
-@end
--- Example/Pods/Target Support Files/Pods-macOS Demo/Pods-macOS Demo-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-macOS Demo/Pods-macOS Demo-frameworks.sh
@@ -163,13 +163,11 @@ strip_invalid_archs() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire-macOS/Alamofire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/DropmarkSDK-macOS/DropmarkSDK.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/KeychainSwift-macOS/KeychainSwift.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/PromiseKit.root-CorePromise-Foundation/PromiseKit.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire-macOS/Alamofire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/DropmarkSDK-macOS/DropmarkSDK.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/KeychainSwift-macOS/KeychainSwift.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/PromiseKit.root-CorePromise-Foundation/PromiseKit.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
