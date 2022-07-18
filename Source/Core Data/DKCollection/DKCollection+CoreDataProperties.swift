3fbb1a10ff44f1f3e76f06f63252d66d43898a33
Alex Givens <mail@alexgivens.com>
2022-07-18 16:15:01 -0400

    Update DKCollection to revert scalars back to NSNumber wrappers for Swift optional support.:

--- Source/Core Data/DKCollection/DKCollection+CoreDataProperties.swift
+++ Source/Core Data/DKCollection/DKCollection+CoreDataProperties.swift
@@ -19,22 +19,22 @@ extension DKCollection {
     @NSManaged public var createdAt: Date?
     @NSManaged public var customDomain: URL?
     @NSManaged public var descriptionText: String?
-    @NSManaged public var id: Int64
-    @NSManaged public var isArchived: Bool
-    @NSManaged public var isHighlighted: Bool
-    @NSManaged public var itemsTotalCount: Int32
+    @NSManaged public var id: NSNumber?
+    @NSManaged public var isArchived: NSNumber?
+    @NSManaged public var isHighlighted: NSNumber?
+    @NSManaged public var itemsTotalCount: NSNumber?
     @NSManaged public var kindRaw: String?
     @NSManaged public var lastAccessedAt: Date?
     @NSManaged public var name: String?
     @NSManaged public var shortURL: URL?
-    @NSManaged public var showLabels: Bool
-    @NSManaged public var sort: Int32
+    @NSManaged public var showLabels: NSNumber?
+    @NSManaged public var sort: NSNumber?
     @NSManaged public var sortByRaw: String?
     @NSManaged public var sortOrderRaw: String?
     @NSManaged public var thumbnail: URL?
     @NSManaged public var updatedAt: Date?
     @NSManaged public var url: URL?
-    @NSManaged public var usersTotalCount: Int32
+    @NSManaged public var usersTotalCount: NSNumber?
     @NSManaged public var viewModeRaw: String?
     @NSManaged public var items: NSSet?
 
